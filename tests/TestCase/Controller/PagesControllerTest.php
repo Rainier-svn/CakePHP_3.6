abc5650bb5e0127b489869ae17fb3dacc159b8f8
Rainier <rainier@seedanalytics.info>
2018-10-19 08:48:31 +0200

    Update tests/TestCase/Controller/PagesControllerTest.php

--- tests/TestCase/Controller/PagesControllerTest.php
+++ tests/TestCase/Controller/PagesControllerTest.php
@@ -14,13 +14,13 @@
  */
 namespace App\Test\TestCase\Controller;
 
-use App\Controller\PagesController;
-use Cake\Core\App;
 use Cake\Core\Configure;
-use Cake\Http\Response;
-use Cake\Http\ServerRequest;
 use Cake\TestSuite\IntegrationTestCase;
-use Cake\View\Exception\MissingTemplateException;
+// use App\Controller\PagesController;
+// use Cake\Core\App;
+// use Cake\Http\Response;
+// use Cake\Http\ServerRequest;
+// use Cake\View\Exception\MissingTemplateException;
 
 /**
  * PagesControllerTest class
